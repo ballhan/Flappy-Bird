e7b620a46a81265e3a47b382dffad9f8165279e3
Bohan Chen <bohanchen81@yahoo.com>
2018-09-21 14:10:24 -0400

    Add files via upload

--- Js/game.js
+++ Js/game.js
@@ -1,116 +1,76 @@
-var myGamePiece;
+var myBird;
 var myObstacles = [];
 var myScore;
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+const canvasHeight = canvas.height = 0.8 * window.innerHeight;
+const canvasWidth = canvas.width = 0.5 * canvasHeight;
+const birdWidth = 0.02 * window.innerHeight;
+const birdHeight = birdWidth;
+const birdColor = "#FF0000";
 
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece.gravity = 0.05;
-    myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+    myBird = new bird();
+    myBird.gravity = 0.05;
     myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
-        },
+        updateGameArea();
+    },
     clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        c.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.score = 0;
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
+function bird() {
+    this.bodyWidth = birdWidth;
+    this.bodyHeight = birdHeight;
+    this.bodyColor = birdColor;
     this.speedY = 0;    
-    this.x = x;
-    this.y = y;
+    //spawn X,Y
+    this.x = 0.2 * canvasWidth;
+    this.y = 0.3 * canvasHeight;
     this.gravity = 0;
     this.gravitySpeed = 0;
-    this.update = function() {
-        ctx = myGameArea.context;
-        if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
-            ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
+    this.draw = function() {
+        c.fillStyle = this.bodyColor;
+        c.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.newPos = function() {
+    this.move = function() {
+        var buttomMargin = canvasHeight - birdHeight;
         this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
         this.y += this.speedY + this.gravitySpeed;
-        this.hitBottom();
-    }
-    this.hitBottom = function() {
-        var rockbottom = myGameArea.canvas.height - this.height;
-        if (this.y > rockbottom) {
-            this.y = rockbottom;
+        if (this.y >= buttomMargin) {
+            this.y = buttomMargin;
             this.gravitySpeed = 0;
         }
     }
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
+    this.crashWith = function(pillarArray) {
+        var x1 = this.x;
+        var x2 = this.x + this.bodyWidth;
+        var y1 = this.y;
+        var y2 = this.y + this.bodyHeight;
         var otherleft = otherobj.x;
         var otherright = otherobj.x + (otherobj.width);
         var othertop = otherobj.y;
         var otherbottom = otherobj.y + (otherobj.height);
         var crash = true;
         if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-            crash = false;
+            return;
         }
-        return crash;
+    }
+    this.update = function() {
+        this.draw();
+        this.move();
     }
 }
 
 function updateGameArea() {
-    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-    for (i = 0; i < myObstacles.length; i += 1) {
-        if (myGamePiece.crashWith(myObstacles[i])) {
-            return;
-        } 
-    }
-    myGameArea.clear();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(150)) {
-        x = myGameArea.canvas.width;
-        minHeight = 20;
-        maxHeight = 200;
-        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-        minGap = 50;
-        maxGap = 200;
-        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(10, height, "green", x, 0));
-        myObstacles.push(new component(10, x - height - gap, "green", x, height + gap));
-    }
-    for (i = 0; i < myObstacles.length; i += 1) {
-        myObstacles[i].x += -1;
-        myObstacles[i].update();
-    }
-    myScore.text="SCORE: " + myGameArea.frameNo;
-    myScore.update();
-    myGamePiece.newPos();
-    myGamePiece.update();
+    myBird.update();
 }
 
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}
-
-function accelerate(n) {
-    myGamePiece.gravity = n;
+function accelerate(n, bird) {
+    bird.gravity = n;
 }
